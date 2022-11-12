5fc6d93fc616988341297f1039143161cb6bc7a6
SafaTouil <safa.touil@esprit.tn>
2022-11-12 16:33:18 +0100

    Operateur test commit 1

---
+++ src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
@@ -0,0 +1,70 @@
+package com.esprit.examen.services;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.esprit.examen.entities.Operateur;
+import com.esprit.examen.repositories.OperateurRepository;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OperateurServiceImplTest {
+	
+	@Autowired
+	IOperateurService op;
+
+		
+	@Autowired
+	OperateurRepository operateurRepository ;
+	
+	private static final Logger l = LogManager.getLogger(OperateurServiceImplTest.class);
+
+	
+	@Test()
+	public void testRetrieveAllOperateur () { //Done
+		
+		List<Operateur> listOperateurs = op.retrieveAllOperateurs();
+		assertThat(listOperateurs).size().isGreaterThan(0);
+		op.retrieveAllOperateurs();
+	}
+	
+	@Test
+	public void testaddOperateur() { //Done
+
+		Operateur o = new Operateur("testNom2", "testPrenom2"); //null, "Safa", "Touil", "123", null
+	    Assertions.assertNotNull(o, "Champ vide !!"); //Vérifier si les champs vides
+	    op.addOperateur(o); //Ajouter alors l'opérateur
+			
+	}
+	
+	@Test
+	public void testdeleteOperateur() { //Done
+
+		long id = 2;
+		op.deleteOperateur(id);
+			
+	}
+	
+	@Test
+	public void testretrieveOperateur() { //Done
+    
+		Operateur o = op.retrieveOperateur(Long.valueOf(2));
+		assertNotNull(o);
+		l.info("retrieveOperateur :" + o);
+			
+	}
+} 
+
